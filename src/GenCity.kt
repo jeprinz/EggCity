740a621ab9d1cd2649d45f3e980ff921873d93e3
jacob5555555555 <jacobeliasprinz@gmail.com>
2018-03-31 19:39:04 -0400

    Hi

--- src/GenCity.kt
+++ src/GenCity.kt
@@ -2,7 +2,7 @@ fun genCity(size: Double) : PolygonGraph<Structure>{
     val polyG = PolygonGraph<Structure>(Rtwo())
 
     val city = Blank()
-    val cityOutline = makecity(size, .5, 100, 5, 2.0)
+    val cityOutline = makecity(size, .5, 10, 5, 2.0)
 
     val rTwo = polyG.getNodes().first()
 
