5cba9c84169ed4ed12261ee44167810695d9ebdd
robby <robbyef98@gmail.com>
2018-03-31 10:48:37 -0400

    added comments

--- src/City.kt
+++ src/City.kt
@@ -3,6 +3,12 @@ class City(shap: Polygon) {
 
 }
 
+/**
+ * rad is expected radius of the city
+ * varr is a number between 0 and .5 which determines how much the city's width varries
+ * precpon is how many points define the city
+ * precrand is how many trig functions we use
+ */
 fun makecity(rad: Double, varr: Double, precpon: Int, precrand:Int): City {
     var arl:ArrayList<Point> = ArrayList()
    var arr:Array<Array<Double>> = arrayOf()
