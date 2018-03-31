ae935cade7b941e508298d8bd999f8d2e7556d78
dsmolyak <dsmolyak@gmail.com>
2018-03-31 16:49:06 -0400

    Reformatted robby's files

--- src/Road.kt
+++ src/Road.kt
@@ -1,44 +1,45 @@
-import com.sun.deploy.registration.InstallHints
-
-class Road(shap:Polygon) {
-    val shape:Polygon=shap
+class Road(shap: Polygon) {
+    val shape: Polygon = shap
 }
-fun makeRoad(pla:Polygon, siz:Double):Road {
-var segg: ArrayList<Segment> =pla.segs
-    var i1:Int =0
-    var i2:Int =0
-    while (i1==i2) {
+
+fun makeRoad(pla: Polygon, siz: Double): Road {
+    val segg: ArrayList<Segment> = pla.segs
+    var i1: Int = 0
+    var i2: Int = 0
+    while (i1 == i2) {
         i1 = (Math.random() * segg.size).toInt()
         i2 = (Math.random() * segg.size).toInt()
     }
-    var s1:Segment=segg.get(i1)
-    var s2:Segment=segg.get(i2)
-    var pm1:Point=getPoint(s1);
-    var pm2:Point=getPoint(s2)
-    var slope:Double = getSlop(pm1,pm2)
-    var pm1x:Double=pm1.x-siz*Math.cos(Math.atan(slope))
-    var pm1y:Double=pm1.y-siz*Math.sin(Math.atan(slope))
-    var pm2x:Double=pm2.x+siz*Math.cos(Math.atan(slope))
-    var pm2y:Double=pm2.y+siz*Math.sin(Math.atan(slope))
+    val s1: Segment = segg.get(i1)
+    val s2: Segment = segg.get(i2)
+    val pm1: Point = getPoint(s1);
+    val pm2: Point = getPoint(s2)
+    val slope: Double = getSlop(pm1, pm2)
+    val pm1x: Double = pm1.x - siz * Math.cos(Math.atan(slope))
+    val pm1y: Double = pm1.y - siz * Math.sin(Math.atan(slope))
+    val pm2x: Double = pm2.x + siz * Math.cos(Math.atan(slope))
+    val pm2y: Double = pm2.y + siz * Math.sin(Math.atan(slope))
 
-    var slo=-1.0/getSlop(pm1,pm2)
-    var yd:Double= siz*Math.sin(Math.atan(slo))
-    var xd:Double=siz*Math.cos(Math.atan(slo))
-    var lis:ArrayList<Segment> = ArrayList()
-    lis.add(Segment(Point(pm1x+xd,pm1y+yd),Point(pm1x-xd,pm1y-yd)))
-    lis.add(Segment(Point(pm1x-xd,pm1y-yd),Point(pm2x-xd,pm2y-yd)))
-    lis.add(Segment(Point(pm2x-xd,pm2y-yd),Point(pm2x+xd,pm2y+yd)))
-    lis.add(Segment(Point(pm2x+xd,pm2y+yd),Point(pm1x+xd,pm1y+yd)))
+    val slo = -1.0 / getSlop(pm1, pm2)
+    val yd: Double = siz * Math.sin(Math.atan(slo))
+    val xd: Double = siz * Math.cos(Math.atan(slo))
+    val lis: ArrayList<Segment> = ArrayList()
+    lis.add(Segment(Point(pm1x + xd, pm1y + yd), Point(pm1x - xd, pm1y - yd)))
+    lis.add(Segment(Point(pm1x - xd, pm1y - yd), Point(pm2x - xd, pm2y - yd)))
+    lis.add(Segment(Point(pm2x - xd, pm2y - yd), Point(pm2x + xd, pm2y + yd)))
+    lis.add(Segment(Point(pm2x + xd, pm2y + yd), Point(pm1x + xd, pm1y + yd)))
     print(lis)
 
     return Road(Polygon(lis))
 
 
 }
-fun getSlop(p1:Point,p2:Point):Double{
-    return (p2.y-p1.y)/(p2.x-p1.x)
+
+fun getSlop(p1: Point, p2: Point): Double {
+    return (p2.y - p1.y) / (p2.x - p1.x)
 }
-fun getPoint(seg:Segment):Point{
-    var loc:Double = Math.random()
-    return Point(seg.p1.x+loc*(seg.p2.x-seg.p1.x),seg.p1.y+loc*(seg.p2.y-seg.p1.y))
+
+fun getPoint(seg: Segment): Point {
+    val loc: Double = Math.random()
+    return Point(seg.p1.x + loc * (seg.p2.x - seg.p1.x), seg.p1.y + loc * (seg.p2.y - seg.p1.y))
 }
