3fcfa4aeda89cd37c8b8bc70927fede862fa7433
jacob5555555555 <jacobeliasprinz@gmail.com>
2018-03-31 08:53:37 -0400

    Made graph not bad

--- src/Graph.kt
+++ src/Graph.kt
@@ -1,47 +1,49 @@
 class Graph<N, E> {
 
-    var nodeList : ArrayList<Node<N>> = arrayListOf()
+    var nodeList : ArrayList<Node<N, E>> = arrayListOf()
     var edgeList : ArrayList<Edge<N, E>> = arrayListOf()
 
-    fun addNode(node : N) {
-        nodeList.add(Node(node))
+    fun addNode(node : Node<N, E>) {
+        nodeList.add(node)
     }
 
-    fun removeNode(node : N) {
-        nodeList.remove(Node(node))
+    fun removeNode(node : Node<N, E>) {
+        edgeList.removeAll(node.neighbors)
+        nodeList.remove(node)
     }
 
     fun hasNode(node : N) : Boolean{
-        return getNodes().contains(N)
+        return getNodes().contains(node)
     }
 
     fun addEdge(node1 : N, node2 : N, data :E) {
-        val n1 = Node(node1); val n2 = Node(node2)
+        val n1 = Node<N, E>(node1); val n2 = Node<N,E>(node2)
         if (!nodeList.contains(n1) || !nodeList.contains(n2)) {
             throw IllegalStateException("Expected '$node1' and '$node2' nodes to exist in graph")
         }
-        edgeList.add(Edge(n1, n2, data))
-        n1.neighbors.add(n2)
-        n2.neighbors.add(n1)
+        val edge = Edge(n1, n2, data)
+        edgeList.add(edge)
+        n1.neighbors.add(edge)
+        n2.neighbors.add(edge)
     }
 
     fun getNodes() : Collection<N>{
         return nodeList.map(
-                fun(node : Node<N>) : N{
+                fun(node : Node<N, E>) : N{
                     return node.node
                 }
         )
     }
 
-    data class Node<N>(val node: N) {
+    data class Node<N,E>(val node: N) {
 
-        val neighbors: ArrayList<Node<N>> = arrayListOf()
+        val neighbors: ArrayList<Edge<N, E>> = arrayListOf()
 
         override fun equals(other: Any?): Boolean {
             if (this === other) {return true}
             else if (other?.javaClass != javaClass) return false
             else {
-                return (other as Node<N>).node!! == node
+                return (other as Node<N, E>).node!! == node
             }
         }
 
@@ -50,6 +52,6 @@ class Graph<N, E> {
         }
     }
 
-    data class Edge<N, E>(val node1: Node<N>, val node2: Node<N>, val data : E)
+    data class Edge<N, E>(val node1: Node<N, E>, val node2: Node<N, E>, val data : E)
 
 }
